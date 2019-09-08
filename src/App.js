4f7271660d58e876b12c82d04cf53d43c53d5b26
“VitaliyNickishin” <nickishin34@gmail.com>
2019-09-08 18:27:52 +0300

    Navbar is ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Navbars from './components/Navbar/Navbars'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars />
+      <Switch>
+        {/* <Route exact path="/" component={Главная} />
+        <Route exact path="/туры" component={Туры} />
+        <Route exact path="/страны" component={Страны} />
+        <Route component={Error} /> */}
+      </Switch>
     </div>
   );
 }
