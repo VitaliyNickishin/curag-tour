c814028fd3d79b40434fc7e6d062d848742ddab4
“VitaliyNickishin” <nickishin34@gmail.com>
2019-10-05 17:02:24 +0300

    AroundWorld is ready for page Company and media

--- src/pages/Company/Company.js
+++ src/pages/Company/Company.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+// import { Container } from 'react-bootstrap'
 import Hero from '../../components/Hero/Hero'
 import Bunner from '../../components/Bunner/Bunner'
+import AroundWorld from '../../components/AroundWorld/AroundWorld'
 
 export default function Company() {
  return (
   <>
    <Hero name="about" title="О нашей компании">
-    <Container>
-     <Bunner/>
-    </Container>
+    <Bunner/>
    </Hero>
+   <AroundWorld/>
   </>
  )
 }
