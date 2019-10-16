b1206a23c51bf69e69fdb21b1cdc61688694c462
“VitaliyNickishin” <nickishin34@gmail.com>
2019-10-16 18:39:35 +0300

    comp. FeedbackCard completed

--- src/components/FeedbackCard/FeedbackCard.js
+++ src/components/FeedbackCard/FeedbackCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {FaStar} from 'react-icons/fa'
+// import '../../fontello.scss'
 // import shape from '../../img/feedback/feedback-shape.png'
 import './FeedbackCard.sass'
 
@@ -7,20 +9,28 @@ export default class FeedbackCard extends Component {
   const {url,name,description} = this.props.item
   return (
    <article className="feedback-card">
-    <div className="card-img">
-     <img
-      src={url} 
-      alt="photoimg"
-     />
-     {/* <img src={shape} alt="" className="shape"/> */}
+    <div className="img-wrap">
+     <div className="face">
+      <img
+       src={url} 
+       alt="photoimg"
+      />
+      {/* <img src={shape} alt="" className="shape"/> */}
      </div>
+    </div>
      <div className="stars">
-      
+      <FaStar className="stars-icon"/>
+      <FaStar className="stars-icon"/>
+      <FaStar className="stars-icon"/>
+      <FaStar className="stars-icon"/>
+      <FaStar className="stars-icon"/>
+      {/* <i className="icon-star-empty-1 stars-icon"></i> */}
      </div>
-     <div className="feedback-info">
+     <div className="info">
       <h3>{name}</h3>
       <p>{description}</p>
      </div>
+     <button type="button" className="read-more">читать больше</button>
      
    </article>
   )
